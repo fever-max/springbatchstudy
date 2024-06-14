715a68eeaf21fe9f0ce041be9a1c4a21934d5fc4
hyemin <fever_max@naver.com>
2024-06-14 14:15:08 +0900

    Feat: 배치 실행 전후 리스너 기능

---
+++ src/main/java/com/example/springbatchstudy/job/jobListener/JobListenerConfig.java
@@ -0,0 +1,56 @@
+package com.example.springbatchstudy.job.jobListener;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobListenerConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job jobListenerJob(Step jobListenerStep) {
+        return jobBuilderFactory.get("jobListenerStep")
+                .incrementer(new RunIdIncrementer())
+                .listener(new JobLoggerListener())
+                .start(jobListenerStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step jobListenerStep(Tasklet jobListenerTasklet) {
+        return stepBuilderFactory.get("jobListenerStep")
+                .tasklet(jobListenerTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet jobListenerTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Jop Listener Tasklet");
+                return RepeatStatus.FINISHED;
+                // throw new Exception("실패 체크");
+            }
+        };
+    }
+
+}
