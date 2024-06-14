96ccf684f656ed380a3d36e981b76eaec49ff26f
hyemin <fever_max@naver.com>
2024-06-14 13:02:17 +0900

    Init: 스프링배치 기본 설정 테스트

---
+++ src/main/java/com/example/springbatchstudy/SpringbatchstudyApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbatchstudy;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class SpringbatchstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchstudyApplication.class, args);
+	}
+
+}
